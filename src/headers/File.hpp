39cc7b4017068bf95b64626ff9583db08822f574
Almir Neto <almirneto338@gmail.com>
2023-06-26 23:52:31 -0300

    Creating file reader

--- src/headers/File.hpp
+++ src/headers/File.hpp
@@ -3,12 +3,16 @@
 #define FILE_H
 
 #include <string>
+#include <fstream>
+#include <sstream>
 
 class File {
    public:
     File();
     File(std::string);
     ~File();
+    void setPath(std::string path);
+    char* getContent();
 
    private:
     std::string path;
